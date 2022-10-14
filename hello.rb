f2d25e8555aaf2411154c28290a44574c1289de2
JoshuaPaino <joshua.paino@gmail.com>
2022-10-14 18:44:14 -0500

    Add Sinatra routes

--- hello.rb
+++ hello.rb
@@ -14,6 +14,11 @@ get route do
     "\"#{route}\" is my url path, using GET\n"
   end
 end
+
+get '/halt' do
+    "What do you think happens with this text?"
+    halt 500, {'Content-Type' => 'text/plain'}, 'you halted me!'
+end
 # your url path should be similiar
 # get '/:name' do
 #     "#{params[:name]} #{params[:describe]} Sinatra!"
